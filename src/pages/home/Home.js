91ae4968543487123fb247105c9d473dd55f0a10
minji0123 <sso02027@naver.com>
2022-11-26 23:21:25 +0900

    입력 form 제작

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,19 @@
 /* eslint-disable*/
 
+import DiaryForm from './DiaryForm'
 import styles from './Home.module.css'
 
-export default function Home(){
+
+export default function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <main className={styles.cont}>
+            <aside className={styles.side_menu}>
+                <DiaryForm></DiaryForm>
+            </aside>
+            <ul className={styles.content_list}>
+                dairy list
+            </ul>
+
+        </main>
     )
 }
